124a93ad29587e69c106a623e701361940cf8920
Ahmed Ben Sassi <bensassi.ahmed@gmail.com>
2025-03-31 17:52:19 +0100

    0.1

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, File, UploadFile, HTTPException
+import pretty_midi
+from io import BytesIO
+
+app = FastAPI()
+
+@app.post("/")
+async def analyze_midi(midi_file: UploadFile = File(...)):
+    try:
+        content = await midi_file.read()
+        midi_data = pretty_midi.PrettyMIDI(BytesIO(content))
+        tempos = midi_data.get_tempo_changes()[1]
+        return {"tempo": tempos[0] if tempos else 120}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
